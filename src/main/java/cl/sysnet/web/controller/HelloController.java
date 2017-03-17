2b59939b5097f4c757c484b449f83948f30f89cd
Ivan A. Flores C <ivan.flores@zweicom.com>
2017-03-16 23:06:35 -0300

    Se agrega lectura de archivo

--- src/main/java/cl/sysnet/web/controller/HelloController.java
+++ src/main/java/cl/sysnet/web/controller/HelloController.java
@@ -1,7 +1,9 @@
 package cl.sysnet.web.controller;
 
-import javax.annotation.PostConstruct;
+import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +12,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class HelloController {
-
-	@PostConstruct
-	public void init() {
-		log.info("Cargamos Controller !!! >>>>>>>>>>>>>>");
-	}
+	
+	@Value("${file.path}")
+	private Resource resource;
 
 	@RequestMapping("/")
 	public String index() {
+		log.info("Leemos el archivo");
+		try {
+            resource.getFile();
+            log.info("Found the resource " + resource.getFilename());
+        } catch (IOException ex) {
+        	log.error(ex.toString());
+        }
+		
 		return "Greetings from Spring Boot!";
 	}
 
